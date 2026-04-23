aa6b388abc0f7d2903383b9fd83a819101ce316c
Joep Meindertsma <joep@ontola.io>
2026-04-23 21:51:28 +0200

    Better momentum, crosshair

--- scripts/network_manager.gd
+++ scripts/network_manager.gd
@@ -4,10 +4,82 @@ const PORT := 27015
 const MAX_PLAYERS := 8
 const DEFAULT_ADDRESS := "127.0.0.1"
 
+const DISCOVERY_PORT := 27016
+const DISCOVERY_INTERVAL := 1.5
+
 signal player_list_changed
+signal game_discovered(games: Dictionary)
 
 var players: Dictionary = {}
 var local_player_name: String = "Player"
+
+var discovered_games: Dictionary = {} # addr -> {name, count, time}
+var _udp_peer: PacketPeerUDP = null
+var _discovery_timer: float = 0.0
+
+func _process(delta: float) -> void:
+	if _udp_peer:
+		if multiplayer.is_server():
+			_discovery_timer += delta
+			if _discovery_timer >= DISCOVERY_INTERVAL:
+				_discovery_timer = 0.0
+				_broadcast_game_status()
+		else:
+			_listen_for_games()
+			_cleanup_old_games(delta)
+
+func start_discovery() -> void:
+	discovered_games.clear()
+	_udp_peer = PacketPeerUDP.new()
+	if multiplayer.is_server():
+		_udp_peer.set_broadcast_enabled(true)
+		_udp_peer.set_dest_address("255.255.255.255", DISCOVERY_PORT)
+	else:
+		var err := _udp_peer.bind(DISCOVERY_PORT)
+		if err != OK:
+			push_error("Failed to bind discovery port: %s" % err)
+			_udp_peer = null
+
+func stop_discovery() -> void:
+	if _udp_peer:
+		_udp_peer.close()
+		_udp_peer = null
+	discovered_games.clear()
+
+func _broadcast_game_status() -> void:
+	if not _udp_peer: return
+	var data := {
+		"name": local_player_name,
+		"players": players.size(),
+		"max": MAX_PLAYERS
+	}
+	var msg := JSON.stringify(data)
+	_udp_peer.put_packet(msg.to_utf8_buffer())
+
+func _listen_for_games() -> void:
+	while _udp_peer and _udp_peer.get_available_packet_count() > 0:
+		var packet: PackedByteArray = _udp_peer.get_packet()
+		var addr: String = _udp_peer.get_packet_ip()
+		var msg: String = packet.get_string_from_utf8()
+		var json: Variant = JSON.parse_string(msg)
+		if json is Dictionary:
+			discovered_games[addr] = {
+				"name": json.get("name", "Unknown"),
+				"players": json.get("players", 1),
+				"max": json.get("max", MAX_PLAYERS),
+				"time": Time.get_ticks_msec()
+			}
+			game_discovered.emit(discovered_games)
+
+func _cleanup_old_games(_delta: float) -> void:
+	var now := Time.get_ticks_msec()
+	var changed := false
+	for addr in discovered_games.keys():
+		if now - discovered_games[addr].time > 5000:
+			discovered_games.erase(addr)
+			changed = true
+	if changed:
+		game_discovered.emit(discovered_games)
 
 func host_game(player_name: String) -> bool:
 	local_player_name = player_name
@@ -21,6 +93,8 @@ func host_game(player_name: String) -> bool:
 	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
 	players[1] = player_name
 	player_list_changed.emit()
+
+	start_discovery() # Start broadcasting as soon as we host
 	return true
 
 func join_game(address: String, player_name: String) -> bool:
@@ -34,6 +108,8 @@ func join_game(address: String, player_name: String) -> bool:
 	multiplayer.connected_to_server.connect(_on_connected_to_server)
 	multiplayer.connection_failed.connect(_on_connection_failed)
 	multiplayer.server_disconnected.connect(_on_server_disconnected)
+
+	stop_discovery() # Stop looking once we join one
 	return true
 
 func auto_connect(player_name: String) -> bool:
@@ -87,6 +163,7 @@ func _launch_args() -> Array[String]:
 	return out
 
 func leave_game() -> void:
+	stop_discovery()
 	if multiplayer.multiplayer_peer:
 		multiplayer.multiplayer_peer.close()
 		multiplayer.multiplayer_peer = null
@@ -117,6 +194,7 @@ func _on_connection_failed() -> void:
 	multiplayer.multiplayer_peer = null
 
 func _on_server_disconnected() -> void:
+	stop_discovery()
 	players.clear()
 	multiplayer.multiplayer_peer = null
 	player_list_changed.emit()
